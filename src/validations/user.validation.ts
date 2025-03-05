467afa5756dba32e305218c2ba41f90096964f31
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-03-05 12:50:45 +0700

    feat(user): Add field office ID to user validation

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -4,5 +4,8 @@ export const userSchema = z.object({
     username :z.string().min(3, {message : "Username must be at least 3 characters long"}),
     email :z.string().email("Invalid email format"),
     password :z.string().min(6, {message : "Password must be at least 6 characters long"}),
-    roleId :z.number().min(1, {message : "Role ID must be at least 1"})
+    roleId :z.number().min(1, {message : "Role ID must be at least 1"}),
+    job_position_id :z.number().min(1, {message : "Job position ID must be at least 1"}),
+    office_id :z.number().min(1, {message : "Office ID must be at least 1"})
+    
 });
